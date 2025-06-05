72f0f6315fced7f8fdb49d83d38bfb7adb2ab1cf
Fred <huertaserick@yahoo.ie>
2025-06-05 14:26:01 +0100

    chore: add id to data for 'loves'

--- app/about/loves/page.tsx
+++ app/about/loves/page.tsx
@@ -4,16 +4,26 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import loves from "@/data/loves.json";
 
 const Adore = () => {
   return (
     <Accordion type="single" collapsible className="w-full max-w-xl">
       <AccordionItem value="item-1">
-        <AccordionTrigger className="text-lg font-bold text-black">
+        <AccordionTrigger
+          aria-label="Toggle the list of things I love"
+          className="text-lg font-bold text-black"
+        >
           😻 Everything I Love! 😍
         </AccordionTrigger>
         <AccordionContent className="text-base text-gray-700 space-y-3 mt-2">
-          <p></p>
+          {loves.map((item) => (
+            <p
+              key={item.id}
+              className="opacity-0 animate-fadeIn"
+              dangerouslySetInnerHTML={{ __html: item.answer }}
+            />
+          ))}
         </AccordionContent>
       </AccordionItem>
     </Accordion>
