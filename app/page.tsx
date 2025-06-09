ce8fd3f77109911a743a3f5f678d28b44aef2a68
Fred <huertaserick@yahoo.ie>
2025-06-09 14:08:37 +0100

    feat: changed h1 tag to use gsap from react bits ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
-// app/page.tsx or app/messages/page.tsx
-
+"use client";
 import MessageBoard from "@/components/MessageBoard/MessageBoard";
+import SplitText from "@/components/react-bits-ui/SplitText";
+
+const handleAnimationComplete = () => {
+  console.log("All letters have animated!");
+};
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-pink-100 to-yellow-100 p-5 max-w-xl">
-      <h1 className="text-3xl font-bold mb-6 text-center break-words px-10 py-3">
+      {/* <h1 className="text-3xl font-bold mb-6 text-center break-words px-10 py-3">
         🎂 Birthday Messages Wall🎈
-      </h1>
+      </h1> */}
+      <div className="flex justify-center py-5">
+        <SplitText
+          text="🎂 Birthday Messages Wall🎈"
+          className="text-2xl font-semibold text-center"
+          delay={100}
+          duration={0.4}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="center"
+          onLetterAnimationComplete={handleAnimationComplete}
+        />
+      </div>
+
       <MessageBoard />
     </main>
   );
